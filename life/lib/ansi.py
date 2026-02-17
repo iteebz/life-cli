b3fa4893a349971e4bd98484cf8ac025c4587f21
tyson <tyson.chan@proton.me>
2026-02-18 08:22:28 +1100

    refactor: remove PERSONA_COLORS, md_to_ansi dead code from ansi.py

--- life/lib/ansi.py
+++ life/lib/ansi.py
@@ -1,12 +1,3 @@
-import re
-
-PERSONA_COLORS = {
-    "roast": "\033[91m",
-    "pepper": "\033[93m",
-    "kim": "\033[94m",
-}
-
-
 class ANSI:
     BOLD = "\033[1m"
     DIM = "\033[2m"
@@ -23,37 +14,3 @@ class ANSI:
     RESET = "\033[0m"
 
     POOL = [RED, BLUE, MAGENTA, CYAN, YELLOW, GREEN]
-
-
-def md_to_ansi(text: str) -> str:
-    """Convert markdown formatting to ANSI escape codes."""
-
-    lines = []
-    for line in text.split("\n"):
-        formatted = line
-
-        formatted = re.sub(r"\*\*(.+?)\*\*", rf"{ANSI.BOLD}\1{ANSI.RESET}", formatted)
-
-        formatted = re.sub(r"__(.+?)__", rf"{ANSI.BOLD}\1{ANSI.RESET}", formatted)
-
-        formatted = re.sub(r"\*(.+?)\*", rf"{ANSI.ITALIC}\1{ANSI.RESET}", formatted)
-
-        formatted = re.sub(r"_(.+?)_", rf"{ANSI.ITALIC}\1{ANSI.RESET}", formatted)
-
-        formatted = re.sub(r"`(.+?)`", rf"{ANSI.CYAN}\1{ANSI.RESET}", formatted)
-
-        if formatted.startswith("# "):
-            formatted = f"{ANSI.BOLD}{ANSI.MAGENTA}{formatted[2:]}{ANSI.RESET}"
-        elif formatted.startswith("## "):
-            formatted = f"{ANSI.BOLD}{ANSI.BLUE}{formatted[3:]}{ANSI.RESET}"
-        elif formatted.startswith("### "):
-            formatted = f"{ANSI.BOLD}{formatted[4:]}{ANSI.RESET}"
-
-        if formatted.lstrip().startswith("- "):
-            indent = len(formatted) - len(formatted.lstrip())
-            bullet = f"{ANSI.GREEN}•{ANSI.RESET}"
-            formatted = " " * indent + bullet + formatted[indent + 1 :]
-
-        lines.append(formatted)
-
-    return "\n".join(lines)
