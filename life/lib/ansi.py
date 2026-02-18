0d4f66adc890aa7395c1cc2b0364e90e14b5fad5
tyson <tyson.chan@proton.me>
2026-02-18 18:28:54 +1100

    feat(colors): expand color pool from 9 to 20 soft colors
    Add 11 new soft colors (pink, lavender, coral, lime, sky, apricot, mint, berry, gold, sage, indigo) to provide better tag color differentiation as the number of tags grows.

--- life/lib/ansi.py
+++ life/lib/ansi.py
@@ -24,6 +24,17 @@ class ANSI:
     SOFT_RED = "\033[38;5;167m"
     SOFT_PURPLE = "\033[38;5;141m"
     SOFT_TEAL = "\033[38;5;72m"
+    SOFT_PINK = "\033[38;5;175m"
+    SOFT_LAVENDER = "\033[38;5;146m"
+    SOFT_CORAL = "\033[38;5;209m"
+    SOFT_LIME = "\033[38;5;154m"
+    SOFT_SKY = "\033[38;5;117m"
+    SOFT_APRICOT = "\033[38;5;216m"
+    SOFT_MINT = "\033[38;5;122m"
+    SOFT_BERRY = "\033[38;5;163m"
+    SOFT_GOLD = "\033[38;5;185m"
+    SOFT_SAGE = "\033[38;5;151m"
+    SOFT_INDIGO = "\033[38;5;99m"
 
     POOL: ClassVar[list[str]] = [
         SOFT_ORANGE,
@@ -35,4 +46,15 @@ class ANSI:
         SOFT_RED,
         SOFT_PURPLE,
         SOFT_TEAL,
+        SOFT_PINK,
+        SOFT_LAVENDER,
+        SOFT_CORAL,
+        SOFT_LIME,
+        SOFT_SKY,
+        SOFT_APRICOT,
+        SOFT_MINT,
+        SOFT_BERRY,
+        SOFT_GOLD,
+        SOFT_SAGE,
+        SOFT_INDIGO,
     ]
